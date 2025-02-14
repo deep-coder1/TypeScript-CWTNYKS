08db0daa7bca4ba92973d1547c5d1f21eb13fda2
deep-coder1 <ndktbd@gmail.com>
2025-02-14 20:28:58 +0530

    Typescript vs Javascript intro

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+let a = 1;
+a = 'Coding Technyks';
+
+function add(a, b) {
+    return a + b; // no type checking
+}
---
+++ TS/index.ts
@@ -0,0 +1,8 @@
+let a1: string = '1';
+a1 = '10';
+
+function add1(a: number, b: number): number {
+    return a + b; // type checking
+}
+
+add1(1, 2);
